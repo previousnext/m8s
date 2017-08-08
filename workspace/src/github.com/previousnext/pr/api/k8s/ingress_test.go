2c3d2c198cff0b31b5180b8f0b7267d419deb8af
Nick Schuch <nick@myschuch.com>
2017-08-08 16:06:00 +1000

    Adds a test for Ingress()

---
+++ workspace/src/github.com/previousnext/pr/api/k8s/ingress_test.go
@@ -0,0 +1,70 @@
+package k8s
+
+import (
+	"testing"
+
+	pb "github.com/previousnext/pr/pb"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	extensions "k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
+)
+
+func TestIngress(t *testing.T) {
+	obj := &pb.BuildRequest{
+		Metadata: &pb.Metadata{
+			Name: "pr1",
+			Domains: []string{
+				"pr1.example.com",
+				"pr1.example2.com",
+			},
+		},
+	}
+
+	want := &extensions.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "test",
+			Name:      "pr1",
+		},
+		Spec: extensions.IngressSpec{
+			Rules: []extensions.IngressRule{
+				{
+					Host: "pr1.example.com",
+					IngressRuleValue: extensions.IngressRuleValue{
+						HTTP: &extensions.HTTPIngressRuleValue{
+							Paths: []extensions.HTTPIngressPath{
+								{
+									Path: "/",
+									Backend: extensions.IngressBackend{
+										ServiceName: "pr1",
+										ServicePort: intstr.FromInt(80),
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					Host: "pr1.example2.com",
+					IngressRuleValue: extensions.IngressRuleValue{
+						HTTP: &extensions.HTTPIngressRuleValue{
+							Paths: []extensions.HTTPIngressPath{
+								{
+									Path: "/",
+									Backend: extensions.IngressBackend{
+										ServiceName: "pr1",
+										ServicePort: intstr.FromInt(80),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	have, err := Ingress("test", obj)
+	assert.Nil(t, err)
+	assert.Equal(t, want, have)
+}
