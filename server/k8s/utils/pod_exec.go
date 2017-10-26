5df0a67fe2807618511ebfe66fcf21ff1d59e9c1
Nick Schuch <nick@myschuch.com>
2017-10-26 15:29:01 +1000

    Fixes 'step' calls via the API since moving to k8s.io/client-go

--- server/k8s/utils/pod_exec.go
+++ server/k8s/utils/pod_exec.go
@@ -3,36 +3,31 @@ package utils
 import (
 	"io"
 
+	remotecommandserver "k8s.io/apimachinery/pkg/util/remotecommand"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/remotecommand"
-	remotecommandserver "k8s.io/apimachinery/pkg/util/remotecommand"
 )
 
 // PodExec for running commands against a running pod.
 func PodExec(client *kubernetes.Clientset, config *rest.Config, w io.Writer, namespace, name, container, step string) error {
-	cmd := &api.PodExecOptions{
-		Container: container,
-		Stdout:    true,
-		Stderr:    true,
-		Command: []string{
-			"/bin/bash",
-			"-c",
-			step,
-		},
-		TTY: true,
-	}
-
 	opts := remotecommand.StreamOptions{
 		SupportedProtocols: remotecommandserver.SupportedStreamingProtocols,
-		Stdout: w,
-		Stderr: w,
-		Tty:    true,
+		Stdout:             w,
+		Stderr:             w,
+		Tty:                true,
 	}
 
 	// Use the Kubernetes inbuilt client to build a URL endpoint for running our exec command.
-	url := client.Core().RESTClient().Post().Resource("pods").Name(name).Namespace(namespace).SubResource("exec").Param("container", container).VersionedParams(cmd, api.ParameterCodec).URL()
+	req := client.Core().RESTClient().Post().Resource("pods").Name(name).Namespace(namespace).SubResource("exec")
+	req.Param("container", container)
+	req.Param("stdout", "true")
+	req.Param("stderr", "true")
+	req.Param("tty", "true")
+	req.Param("command", "/bin/bash")
+	req.Param("command", "-c")
+	req.Param("command", step)
+	url := req.URL()
 
 	exec, err := remotecommand.NewExecutor(config, "POST", url)
 	if err != nil {
