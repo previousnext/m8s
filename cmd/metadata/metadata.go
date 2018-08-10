9bc5338dec7d836b72207ac7f718a1341f0a6b4c
Nick Santamaria <nick.santamaria@previousnext.com.au>
2018-08-10 16:04:48 +1000

    Added more CircleCI standard environment variables.

--- cmd/metadata/metadata.go
+++ cmd/metadata/metadata.go
@@ -8,22 +8,58 @@ import (
 
 const (
 	// AnnotationBitbucketBranch is an identifier for Bitbucket branch this environment was built from.
-	AnnotationBitbucketBranch = "bitbucket.branch"
+	AnnotationBitbucketBranch = "bitbucket.org/branch"
 
 	// AnnotationBitbucketRepoOwner is an identifier for Bitbucket repository owner.
-	AnnotationBitbucketRepoOwner = "bitbucket.repo.owner"
+	AnnotationBitbucketRepoOwner = "bitbucket.org/repo/owner"
 
 	// AnnotationBitbucketRepoName is an identifier for Bitbucket repository this environment was built from.
-	AnnotationBitbucketRepoName = "bitbucket.repo.name"
+	AnnotationBitbucketRepoName = "bitbucket.org/repo/name"
+
+	// AnnotationCircleCIRepositoryUsername is an identifier for CircleCI repository the environment was built from.
+	AnnotationCircleCIRepositoryUsername = "circleci.com/project/username"
+
+	// AnnotationCircleCIRepositoryName is an identifier for CircleCI repository the environment was built from.
+	AnnotationCircleCIRepositoryName = "circleci.com/project/reponame"
 
 	// AnnotationCircleCIRepositoryURL is an identifier for CircleCI repository the environment was built from.
-	AnnotationCircleCIRepositoryURL = "circleci.repository.url"
+	AnnotationCircleCIRepositoryURL = "circleci.com/repository/url"
 
 	// AnnotationCircleCIPRNumber is an identifier for CircleCI pull request the environment was built from.
-	AnnotationCircleCIPRNumber = "circleci.pr.number"
+	AnnotationCircleCIPRNumber = "circleci.com/pr/number"
 
 	// AnnotationCircleCIPRUsername is an identifier for CircleCI pull request submitted by a user.
-	AnnotationCircleCIPRUsername = "circleci.pr.username"
+	AnnotationCircleCIPRUsername = "circleci.com/pr/username"
+
+	// AnnotationCircleCIBuildNum
+	AnnotationCircleCIBuildNum = "circleci.com/build/num"
+
+	// AnnotationCircleCIBuildURL
+	AnnotationCircleCIBuildURL = "circleci.com/build/url"
+
+	// AnnotationCircleCICompareURL
+	AnnotationCircleCICompareURL = "circleci.com/compare_url"
+
+	// AnnotationCircleCISHA1
+	AnnotationCircleCISHA1 = "circleci.com/sha1"
+
+	// AnnotationCircleCIUsername
+	AnnotationCircleCIUsername = "circleci.com/username"
+
+	// AnnotationCircleCIBranch
+	AnnotationCircleCIBranch = "circleci.com/branch"
+
+	// AnnotationCircleCIJob
+	AnnotationCircleCIJob = "circleci.com/job"
+
+	// AnnotationCircleCIWorkflowID
+	AnnotationCircleCIWorkflowID = "circleci.com/workflow/id"
+
+	// AnnotationCircleCIWorkflowJobID
+	AnnotationCircleCIWorkflowJobID = "circleci.com/workflow/job_id"
+
+	// AnnotationCircleCIWorkflowWorkspaceID
+	AnnotationCircleCIWorkflowWorkspaceID = "circleci.com/workflow/workspace_id"
 )
 
 // Annotations are used for attaching metadata to a environment.
@@ -48,12 +84,36 @@ func Annotations(envs []string) ([]*pb.Annotation, error) {
 			annotations = append(annotations, &pb.Annotation{Name: AnnotationBitbucketRepoName, Value: sl[1]})
 		// Check if we have CircleCI environment variables.
 		// https://circleci.com/docs/2.0/env-vars/
-		case "CIRCLE_REPOSITORY_URL":
-			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIRepositoryURL, Value: sl[1]})
 		case "CIRCLE_PR_NUMBER":
 			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIPRNumber, Value: sl[1]})
 		case "CIRCLE_PR_USERNAME":
 			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIPRUsername, Value: sl[1]})
+		case "CIRCLE_BUILD_NUM":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIBuildNum, Value: sl[1]})
+		case "CIRCLE_BUILD_URL":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIBuildURL, Value: sl[1]})
+		case "CIRCLE_COMPARE_URL":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCICompareURL, Value: sl[1]})
+		case "CIRCLE_SHA1":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCISHA1, Value: sl[1]})
+		case "CIRCLE_PROJECT_REPONAME":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIRepositoryName, Value: sl[1]})
+		case "CIRCLE_PROJECT_USERNAME":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIRepositoryUsername, Value: sl[1]})
+		case "CIRCLE_USERNAME":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIUsername, Value: sl[1]})
+		case "CIRCLE_BRANCH":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIBranch, Value: sl[1]})
+		case "CIRCLE_JOB":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIJob, Value: sl[1]})
+		case "CIRCLE_REPOSITORY_URL":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIRepositoryURL, Value: sl[1]})
+		case "CIRCLE_WORKFLOW_ID":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIWorkflowID, Value: sl[1]})
+		case "CIRCLE_WORKFLOW_JOB_ID":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIWorkflowJobID, Value: sl[1]})
+		case "CIRCLE_WORKFLOW_WORKSPACE_ID":
+			annotations = append(annotations, &pb.Annotation{Name: AnnotationCircleCIWorkflowWorkspaceID, Value: sl[1]})
 		}
 	}
 
--- cmd/metadata/metadata_test.go
+++ cmd/metadata/metadata_test.go
@@ -33,6 +33,54 @@ func TestAnnotations(t *testing.T) {
 			Name:  AnnotationCircleCIPRUsername,
 			Value: "nick",
 		},
+		{
+			Name: AnnotationCircleCIBuildNum,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIBuildURL,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCICompareURL,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCISHA1,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIRepositoryName,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIRepositoryUsername,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIUsername,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIBranch,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIJob,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIWorkflowID,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIWorkflowJobID,
+			Value: "xxx",
+		},
+		{
+			Name: AnnotationCircleCIWorkflowWorkspaceID,
+			Value: "xxx",
+		},
 	}
 
 	have, err := Annotations([]string{
@@ -42,6 +90,18 @@ func TestAnnotations(t *testing.T) {
 		"CIRCLE_REPOSITORY_URL=http://example.com",
 		"CIRCLE_PR_NUMBER=1",
 		"CIRCLE_PR_USERNAME=nick",
+		"CIRCLE_BUILD_NUM=xxx",
+		"CIRCLE_BUILD_URL=xxx",
+		"CIRCLE_COMPARE_URL=xxx",
+		"CIRCLE_SHA1=xxx",
+		"CIRCLE_PROJECT_REPONAME=xxx",
+		"CIRCLE_PROJECT_USERNAME=xxx",
+		"CIRCLE_USERNAME=xxx",
+		"CIRCLE_BRANCH=xxx",
+		"CIRCLE_JOB=xxx",
+		"CIRCLE_WORKFLOW_ID=xxx",
+		"CIRCLE_WORKFLOW_JOB_ID=xxx",
+		"CIRCLE_WORKFLOW_WORKSPACE_ID=xxx",
 		"FOO=bar",
 	})
 	assert.Nil(t, err)
